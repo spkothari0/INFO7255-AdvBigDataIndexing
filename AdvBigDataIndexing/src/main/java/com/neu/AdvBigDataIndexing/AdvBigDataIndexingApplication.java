00c14d9c188209c65932c50a6aea7f88e4cd796b
Shreyas Kothari <spkothari0@gmail.com>
2025-02-07 18:20:53 -0500

    initial commit

---
+++ AdvBigDataIndexing/src/main/java/com/neu/AdvBigDataIndexing/AdvBigDataIndexingApplication.java
@@ -0,0 +1,13 @@
+package com.neu.AdvBigDataIndexing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvBigDataIndexingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvBigDataIndexingApplication.class, args);
+	}
+
+}
---
+++ AdvBigDataIndexing/src/test/java/com/neu/AdvBigDataIndexing/AdvBigDataIndexingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neu.AdvBigDataIndexing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvBigDataIndexingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
